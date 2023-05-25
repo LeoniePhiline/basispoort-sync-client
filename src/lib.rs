0b51e491fca9e02d5ecd9744e4f6470523028f69
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2023-05-25 18:28:54 +0200

    feat: Introduce crate features to toggle institutions API client and hosted license provider API client

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 pub use url::Url;
 
 pub mod error;
+
+#[cfg(feature = "hosted-license-provider")]
 pub mod hosted_license_provider;
+
+#[cfg(feature = "institutions")]
 pub mod institutions;
+
 pub mod rest;
 
 pub type Result<T> = std::result::Result<T, Box<crate::error::Error>>;
